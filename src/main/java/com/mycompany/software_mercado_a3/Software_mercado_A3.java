8cd46d808e084b9d5d4d568f791303ad25aeebe7
Iudhi <iudhicunhasantos0@gmail.com>
2026-04-07 19:59:13 -0300

    ini: hello world!

---
+++ src/main/java/com/mycompany/software_mercado_a3/Software_mercado_A3.java
@@ -0,0 +1,8 @@
+package com.mycompany.software_mercado_a3;
+
+public class Software_mercado_A3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
