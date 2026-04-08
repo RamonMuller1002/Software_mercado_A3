d7dd4f3bfc327988fddc8dc6b1620a313d5e1638
mateussfragaa <mateusflofraga@gmail.com>
2026-04-08 10:30:16 -0300

    refactor(estrutura): reorganiza pacotes e define nova classe principal
    - remove pacote padrão my.company
    - cria os pacotes 'principal' e 'modelos'
    - renomeia classe Main para Principal
    - atualiza classe de execução para principal.Principal
    - ajusta configuração de Run do projeto

--- src/main/java/com/mycompany/software_mercado_a3/Software_mercado_A3.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.software_mercado_a3;
-
-public class Software_mercado_A3 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!"); // testado netbeans
-    }
-}
