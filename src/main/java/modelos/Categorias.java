a40182506d92aa88afd18a4ceaa1b53aad6a47ec
Iudhi <iudhicunhasantos0@gmail.com>
2026-04-23 16:31:16 -0300

    feat: criação da classe Categoria com os contrutores gets e sets

--- src/main/java/modelos/Categorias.java
+++ src/main/java/modelos/Categorias.java
@@ -3,4 +3,43 @@ import enums.*;
 public class Categorias {
 
     private String C_nome;
+    private Embalagem embalagens;
+    private Tamanho tamanho;
+    
+    public Categorias(){
+    }
+
+    public Categorias(String C_nome, Embalagem embalagens, Tamanho tamanho) {
+        this.C_nome = C_nome;
+        this.embalagens = embalagens;
+        this.tamanho = tamanho;
+    }
+
+    public String getC_nome() {
+        return C_nome;
+    }
+
+    public Embalagem getEmbalagens() {
+        return embalagens;
+    }
+
+    public Tamanho getTamanho() {
+        return tamanho;
+    }
+
+    public void setC_nome(String C_nome) {
+        this.C_nome = C_nome;
+    }
+
+    public void setEmbalagens(Embalagem embalagens) {
+        this.embalagens = embalagens;
+    }
+
+    public void setTamanho(Tamanho tamanho) {
+        this.tamanho = tamanho;
+    }
+    
+    
+    
 }
+
